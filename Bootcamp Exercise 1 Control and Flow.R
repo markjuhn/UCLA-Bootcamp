f0ca86201b087f4e2cbfc713bb71246a8eb8ca13
markjuhn <markjuhn@gmail.com>
2016-09-07 16:41:00 -0700

    Finished Ex 5

--- Bootcamp Exercise 1 Control and Flow.R
+++ Bootcamp Exercise 1 Control and Flow.R
@@ -39,3 +39,16 @@ for(i in 1:length(years)){
 }
 ##NOT SURE ABOUT THIS YET
 
+## Exercise 5
+bankAccounts <- c(10, 9.2, 5.6, 3.7, 8.8, 0.5);
+
+#Now look at the error message the following lines of code produce. Can you think of a way to modify this loop so that the loop will compund the interest?
+compounded <- rep(1, length(bankAccounts))
+interestRate <- 0.0125;
+for (i in 1:length(bankAccounts)) {
+  compounded[i] <- interestRate*bankAccounts[i] + bankAccounts[i]; }
+
+#HINT: variables must be initialized before you can perform operations on them
+#HINT 2: look at the rep() function and see if you can use that to initialize a variable that will help you.
+
+
