548f5b458334427876951c0485b8e55e16533d4a
markjuhn <markjuhn@gmail.com>
2016-09-07 16:06:33 -0700

    Finished Ex 2

--- Bootcamp Exercise 1 Control and Flow.R
+++ Bootcamp Exercise 1 Control and Flow.R
@@ -1,8 +1,25 @@
+#Bootcamp Exercise 1: Control and Flow
+
+#Exercise 1
 for (i in 1:9) {
   if (i < 9) {
     cat("\n")
   }
   if (i == 9) {
+    cat("*")
+  }
+}
+
+
+#Line break
+cat("\n")
+
+#Exercise 2
+for (i in 1:10) {
+  if (i < 10) {
+    cat("*&")
+  }
+  if (i == 10) {
     cat("*")
   }
 }
