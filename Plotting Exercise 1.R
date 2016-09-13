87391bdd9393b409412b20fa96ad1c6b1a40ec62
markjuhn <markjuhn@gmail.com>
2016-09-13 16:34:43 -0700

    Finished prob 1

--- Plotting Exercise 1.R
+++ Plotting Exercise 1.R
@@ -1,1 +1,12 @@
 #Plotting Exercise 1
+
+#1
+get_height <- function(n) {
+  heights <- rnorm(n, 69, 10)
+  return(heights)
+}
+## I'm not sure how to assign a variable that will persist outside the function
+## without assigning a global variable, which is bad
+get_height(10)
+
+#2
