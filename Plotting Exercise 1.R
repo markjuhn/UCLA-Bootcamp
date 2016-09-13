c6e7fa4055e318f7a9f4a3daa5cc84de5390bcd8
markjuhn <markjuhn@gmail.com>
2016-09-13 16:53:20 -0700

    Finished up to question 5

--- Plotting Exercise 1.R
+++ Plotting Exercise 1.R
@@ -10,3 +10,27 @@ get_height <- function(n) {
 get_height(10)
 
 #2
+get_height <- function(n) {
+  heights <- rnorm(n, 69, 10)
+  return(mean(heights))
+}
+
+#3
+get_height <- function(n) {
+  heights <- rnorm(n, 69, 10)
+  cat("The mean height is", mean(heights), '\n')
+}
+
+#4
+mean_heights_100 <- numeric(length = 1000)
+for (i in 1:1000) {
+  heights <- rnorm(100, 69, 10)
+  mean_heights_100[i] <-mean(heights)
+}
+
+#5
+mean_heights_1000 <- numeric(length = 1000)
+for (i in 1:1000) {
+  heights <- rnorm(1000, 69, 10)
+  mean_heights_1000[i] <-mean(heights)
+}
