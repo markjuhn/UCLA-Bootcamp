34dd17bdfdcc8dffa308fca14d52ea9d94836ec1
markjuhn <markjuhn@gmail.com>
2016-09-14 16:08:12 -0700

    cleaned up comments

--- Bootcamp_Exercise_1.R
+++ Bootcamp_Exercise_1.R
@@ -41,17 +41,12 @@ for(i in 1:length(years)){
 
 ## Exercise 5
 bankAccounts <- c(10, 9.2, 5.6, 3.7, 8.8, 0.5);
-
-#Now look at the error message the following lines of code produce. Can you think of a way to modify this loop so that the loop will compund the interest?
 compounded <- rep(1, length(bankAccounts))
 interestRate <- 0.0125;
 for (i in 1:length(bankAccounts)) {
   compounded[i] <- interestRate*bankAccounts[i] + bankAccounts[i] 
   }
-
-#HINT: variables must be initialized before you can perform operations on them
-#HINT 2: look at the rep() function and see if you can use that to initialize a variable that will help you.
-
+print(compounded)
 ## Exercise 6
 
 bankAccounts <- c(10, 9.2, 5.6); #define bank accounts here
@@ -94,19 +89,16 @@ for (j in 2015:2020) {
 }
 print(bankAccounts)
 
-##
-
-##NEED TO ASK IF THE STUDENTS HAVE A BEGINING BANK ACCOUNT, OR IF THEY START FROM ZERO
-##ASSUME STARTING FROM ZERO
-## DO LATER
-
 
 #Exercise 8
 x <- 1
 a = 0
-while (x < 18) {
+while (x < 17) {
+  x <- x + 1 #counter?
   a <- a + x
-  x <- x + 1
+  if (x == 17) {
+    print(a)
+  }
 }
 
 #Exercise 9
