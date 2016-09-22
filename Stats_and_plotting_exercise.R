2b1a5b0e2320846cd66e0bc335fce7943e9e3f21
markjuhn <markjuhn@gmail.com>
2016-09-22 11:19:24 -0700

    answered questions 1-3

--- Stats_and_plotting_exercise.R
+++ Stats_and_plotting_exercise.R
@@ -6,12 +6,14 @@ time_zero <- ChickWeight[ChickWeight$Time == 0,]
 plot(time_zero$Diet, time_zero$weight, xlab = "Diet", ylab = "Weight")
 time_zero_model <- lm(formula = time_zero$weight ~ time_zero$Diet)
 anova(time_zero_model)
+#There is no statistical difference between the weight of chicks assigned to different day treatments on day 0
 
 #2
 time_end <- ChickWeight[ChickWeight$Time == 4,]
 plot(time_end$Diet, time_end$weight, xlab = "Diet", ylab = "Weight (gm)")
 time_end_model <- lm(formula = time_end$weight ~ time_end$Diet)
 anova(time_end_model)
+#There is a statistical difference between the weight of chicks assigned to different day treatments on day 21
 
 #3
 data <- ChickWeight
@@ -21,6 +23,7 @@ data <- within (data, {
   Time <- factor(Time)
 })
 summary(aov(weight ~ Diet * Time + Error(Chick), data = data))
+#There is an effect of diet on chick growth, and there is also an affect of the interaction term between diet and time on chick growth
 
 
 #4
